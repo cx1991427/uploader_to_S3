07d68be38a4f6aae7f1c4dc8f2a6d3414495a7cc
Xiang Cao <xcao4@stevens.edu>
2017-06-19 23:07:19 -0400

    add config.json

--- app.js
+++ app.js
@@ -2,15 +2,25 @@
  * Created by Xiang Cao on 6/16/2017.
  */
 var express = require("express");
+var bodyParser = require("body-parser");
+var multer = require("multer");
+var AWS = require("aws-sdk");
 
 var index = require("./routes/index");
 var cat = require("./routes/cat");
 
-require("./config/db");
+// require("./config/db");
+AWS.config.loadFromPath('./config/config.json');
 
 var app = express();
+var s3Bucket = new AWS.S3( { params: {Bucket: 'thriftstorestorage'} } );
+var urlParams = {Bucket: 'thriftstorestorage', Key: 'images.jpg'};
+s3Bucket.getSignedUrl('getObject', urlParams, function(err, url){
+    console.log('the url of the image is', url);
+});
 
-app.use("/cat",cat);
+app.use(bodyParser.urlencoded({extended:false}));
+
 app.use("/",index);
 
 app.listen(3000,function () {
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require("express");
 
 var router = express.Router();
 
-router.get("/",function (req,res) {
-    res.send("welcome!");
+router.get("/upload",function (req,res) {
+    res.sendFile("../"+"public/upload.html");
+});
+
+router.post("/upload",function (req,res) {
+    
 });
 
 module.exports = router;
